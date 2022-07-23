4ef1d5e3b6efc7224b23d7d0e9ab68a342a49726
Sebas <sebastian11ashkar@protonmail.com>
2022-07-22 22:35:47 -0700

    Started a tokenizer

--- src/chat_bot.py
+++ src/chat_bot.py
@@ -8,7 +8,19 @@ import tensorflow
 import random
 import json
 
-#Load the json file
+# Load the json file
 with open("chat_bot_train.json") as file:
     data = json.load(file)
 
+# Tokenize the training data
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        words.extend(nltk.word_tokenize(pattern))
+        docs.append(pattern)
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
