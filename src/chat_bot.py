233df0f079c8e8e2f64a597410c107b931736f7d
Sebas <sebastian11ashkar@protonmail.com>
2022-07-25 20:46:39 -0700

    Chat bot s now able to train json file works now

--- src/chat_bot.py
+++ src/chat_bot.py
@@ -1,26 +1,72 @@
+from pyexpat import model
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy
 import tflearn
-import tensorflow
+import tensorflow as tf
 import random
 import json
 
+nltk.download('punkt')
+
 # Load the json file
-with open("chat_bot_train.json") as file:
+with open("D:\VScode\BridgeHacks-2022-Hackathon\src\chat_bot_train.json") as file:
     data = json.load(file)
 
 # Tokenize the training data
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
-        words.extend(nltk.word_tokenize(pattern))
-        docs.append(pattern)
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels)) if w != "?"]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)    
+
+training = numpy.array(training) # Convert to numpy array
+output = numpy.array(output) 
+
+tf.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(training[0])])
+
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation = "softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+model.fit(training, output, n_epoch=500, batch_size=8, show_metric=True)
+model.savve("model.tflearn")
